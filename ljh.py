16561d7aa87ac0df51e59b8196f833d0ce8d8131
林嘉豪 <825659286@qq.com>
2021-04-27 21:53:12 +0800

    ljhss

--- ljh.py
+++ ljh.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
  
-db = pymysql.connect(host="ljh", user="root", password="123456", database="jindong",charset="utf8")#连接数据库(地址,用户名，密码，数据库名)
+db = pymysql.connect(host="localhost", user="root", password="123", database="jindong",charset="utf8")#连接数据库(地址,用户名，密码，数据库名)
 print("数据库连接成功")
 cur = db.cursor()#取游标 
  
@@ -14,10 +14,12 @@ def parsePage(ilt,html):
     try:
         soup=BeautifulSoup(html,'html.parser')
         plt=soup.find_all('div',attrs={'class':'p-price'})#提取所有class为p-price的标签
+
         tlt=soup.find_all('div',attrs={'class':'p-name p-name-type-2'})
         title=re.findall(r'<em>.*</em>',str(tlt))#需要提取的数据在em标签中，因此用此标签定位
         price=re.findall(r'<i>\d*\.\d*</i>',str(plt))#需要提取的数据在i标签中，因此用此标签定位
-        for i in range(len(title)-1):
+
+        for i in range(len(title)):
             price[i]=re.sub(r'<[^>]+>',"",str(price[i]),re.S)#清除标签
             title[i]=re.sub(r'<[^>]+>',"",str(title[i]),re.S)#清除标签
             ilt.append([price[i],title[i]])
