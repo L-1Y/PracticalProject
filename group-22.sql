414beb6f3c3bcf2b135c9820480ff772343698f3
埃雷 <45099270+L-1Y@users.noreply.github.com>
2021-04-27 23:01:43 +0800

    Add files via upload

---
+++ group-22.sql
@@ -0,0 +1,94 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 4.1.22-community-nt 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `jindong` (
+	`number` int (11),
+	`price` float ,
+	`title` text 
+); 
+insert into `jindong` (`number`, `price`, `title`) values('1','89','青禾佳磊短袖t恤男装夏季衣服男宽松时尚纯色打底衫潮流潮牌上衣男 8712卡其+8712黑色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('2','139','花花公子短袖t恤男衣服2021夏季新款潮圆领男士白小T恤韩版体恤男装时尚打底衫上衣 2110白色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('3','58','特步男装运动短袖t恤衫2021春夏四季新款透气潮流修身男士休闲速干棉显瘦百搭上衣跑步运动健身经典衣服 【店长推荐】白色-logo圆领(速干)四季基础 ...');
+insert into `jindong` (`number`, `price`, `title`) values('4','69','融启 短袖T恤男夏季新款t恤半袖男士潮牌嘻哈衣服男装宽松打底衫五分袖学生运动休闲服饰 2003海蓝色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('5','49.9','南极人T恤男短袖夏季薄款圆领休闲港风青少年学生宽松大码时尚印花半袖纯棉透气夏天男装衣服打底汗衫潮 水绿色(柯基男孩) L【推荐120-130斤左右】');
+insert into `jindong` (`number`, `price`, `title`) values('6','139','【正常发货纯棉4件】短袖t恤男纯棉半截袖衣服圆领休闲宽松潮初中学生印花胖青年大码新品纯色夏季体恤汗衫 四格白+KY蓝+SP灰+海Z黑 L建议120-135斤');
+insert into `jindong` (`number`, `price`, `title`) values('7','99','简柯林外套男春秋装衣服男士修身工装学生上衣青年夹克男春装休闲韩版潮流夹克衫棒球服男 A518白色 L');
+insert into `jindong` (`number`, `price`, `title`) values('8','99','安踏短袖男t恤翻领运动服饰夏季薄款半袖男士宽松速干圆领上衣休闲POLO衫冰丝感跑步网球服男装健身服 -1纯净白色-翻领【店长推荐】 L/175');
+insert into `jindong` (`number`, `price`, `title`) values('9','99','花花公子短袖T恤男士2021夏季新款韩版潮牌休闲修身polo白小t恤打底衫服饰上衣服男装 1812卡其 XL');
+insert into `jindong` (`number`, `price`, `title`) values('10','79','企途短袖T恤男2021新款夏季t恤男装打底衫卫衣长袖秋衣春装男士上衣套装冰丝五分半袖男装夏装 033蓝色+051灰色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('11','139','【三件套】【夹克+T恤+裤子】外套男2021春秋季男装男士休闲运动套装潮流连帽工装夹克外套男上衣服 601卡其黑三件套 XL');
+insert into `jindong` (`number`, `price`, `title`) values('12','79','富贵鸟衣服男士夏季韩版潮流短袖t恤男装休闲上衣夏天青年翻领Polo透气体恤衫 深灰 M');
+insert into `jindong` (`number`, `price`, `title`) values('13','138','（买一送一）春季外套男士夹克2021连帽潮流青年工装上衣服男夹克男外套 1307-2130卡其+内搭T恤 XL');
+insert into `jindong` (`number`, `price`, `title`) values('14','95','快乐嘉人春秋外套男韩版潮流痞帅日系工装夹克ins港风学生秋季上衣服宽松 豆绿色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('15','99','李宁T恤男短袖衣服女运动服饰男士中国篮球polo衫瑜伽半袖体恤男装足球速干健身跑步装备暴汗紧身田径服 基础白-速干店长推荐 L(175)');
+insert into `jindong` (`number`, `price`, `title`) values('16','49','【三件套】卫衣男休闲运动套装2021春秋季男装男士青少年潮流休闲运动服饰情侣装卫衣套装衣服男 175黑三件套 XL');
+insert into `jindong` (`number`, `price`, `title`) values('17','69','【3件装】南极人短袖T恤男夏装新款时尚百搭男士半袖日系宽松体恤大码印花圆领透气衣服男装t恤上衣打底衫 【三件装】80小枫叶白+78小X黑+82小叶黑 XL');
+insert into `jindong` (`number`, `price`, `title`) values('18','128','南极人短袖t恤男纯棉夏季男士半袖衣服圆领休闲潮流打底上衣男生运动T恤衫汗衫3件装 白色H+黑色枫叶+白色英文 XL');
+insert into `jindong` (`number`, `price`, `title`) values('19','85','茄克斯 外套男士2021新款春秋季翻领长袖工装外套夹克男休闲男装修身潮流学生帅气男士潮牌褂子上衣服 深灰色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('20','59','东方狮啸  中国风短袖t恤男士夏季新款薄款透气韩版潮流半袖上衣服休闲宽松国潮五分袖青少年潮牌体恤夏装 白色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('21','108','【上衣+裤子】男款短袖短裤夏天套装男生夏季韩版全套衣服男土一套男装潮流搭配工装裤 198工装裤+D07破洞短袖黑 XL');
+insert into `jindong` (`number`, `price`, `title`) values('22','69','艾伦伯顿 运动套装男健身服速干冰丝短袖夏季宽松薄款训练篮球装备T恤跑步衣服晨跑短裤 科幻宽松两件套【冰丝套装】 XL(145-165)斤');
+insert into `jindong` (`number`, `price`, `title`) values('23','45','（两件套）短袖t恤男套装夏季新款运动服饰男装T恤纯色休闲男士宽松潮流夏天半袖衣服男短袖 套装506白色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('24','59','南极人4件装男士短袖t恤男夏季运动内衣舒适宽松透气休闲弹力修身打底衫套头男式背心汗衫 2黑+2白 XL');
+insert into `jindong` (`number`, `price`, `title`) values('25','59','【纯棉4件】宾宇兄弟t恤男短袖2021年新款韩版修身大码t恤时尚休闲男装上衣服青年半截袖打底衫体恤潮 纯棉套餐7：三杠黑+des卡其+黄杠铁灰+帽子雾...');
+insert into `jindong` (`number`, `price`, `title`) values('26','98','乔丹短袖t恤男宽松2021春夏季新款时尚翻领上衣速干透气休闲运动服圆领半袖POLO衫 【翻领】-1314白色 XL/175');
+insert into `jindong` (`number`, `price`, `title`) values('27','19.89','诗翁 港风t恤男宽松超火ins上衣潮流潮牌短袖男装20新款夏学生衣服男 T恤 T03白色 L');
+insert into `jindong` (`number`, `price`, `title`) values('28','89','青禾佳磊短袖t恤男装夏季衣服男宽松时尚纯色打底衫潮流潮牌上衣男 8712卡其+8712黑色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('29','139','花花公子短袖t恤男衣服2021夏季新款潮圆领男士白小T恤韩版体恤男装时尚打底衫上衣 2110白色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('30','58','特步男装运动短袖t恤衫2021春夏四季新款透气潮流修身男士休闲速干棉显瘦百搭上衣跑步运动健身经典衣服 【店长推荐】白色-logo圆领(速干)四季基础 ...');
+insert into `jindong` (`number`, `price`, `title`) values('31','69','融启 短袖T恤男夏季新款t恤半袖男士潮牌嘻哈衣服男装宽松打底衫五分袖学生运动休闲服饰 2003海蓝色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('32','49.9','南极人T恤男短袖夏季薄款圆领休闲港风青少年学生宽松大码时尚印花半袖纯棉透气夏天男装衣服打底汗衫潮 水绿色(柯基男孩) L【推荐120-130斤左右】');
+insert into `jindong` (`number`, `price`, `title`) values('33','19.89','中国风红旗印花男装T恤衣服五角星爱国青少年男女短袖t恤男 白色 M');
+insert into `jindong` (`number`, `price`, `title`) values('34','139','【正常发货纯棉4件】短袖t恤男纯棉半截袖衣服圆领休闲宽松潮初中学生印花胖青年大码新品纯色夏季体恤汗衫 四格白+KY蓝+SP灰+海Z黑 L建议120-135斤');
+insert into `jindong` (`number`, `price`, `title`) values('35','99','简柯林外套男春秋装衣服男士修身工装学生上衣青年夹克男春装休闲韩版潮流夹克衫棒球服男 A518白色 L');
+insert into `jindong` (`number`, `price`, `title`) values('36','99','安踏短袖男t恤翻领运动服饰夏季薄款半袖男士宽松速干圆领上衣休闲POLO衫冰丝感跑步网球服男装健身服 -1纯净白色-翻领【店长推荐】 L/175');
+insert into `jindong` (`number`, `price`, `title`) values('37','99','大鱼界线 港风t恤男潮流ins情侣装夏装半袖潮牌宽松短袖圆领体恤衫bf原宿学院风字母印花中袖上衣服男 浅灰色 L（120-130斤）');
+insert into `jindong` (`number`, `price`, `title`) values('38','79','花花公子短袖T恤男士2021夏季新款韩版潮牌休闲修身polo白小t恤打底衫服饰上衣服男装 1812卡其 XL');
+insert into `jindong` (`number`, `price`, `title`) values('39','49','企途短袖T恤男2021新款夏季t恤男装打底衫卫衣长袖秋衣春装男士上衣套装冰丝五分半袖男装夏装 033蓝色+051灰色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('40','139','【三件套】【夹克+T恤+裤子】外套男2021春秋季男装男士休闲运动套装潮流连帽工装夹克外套男上衣服 601卡其黑三件套 XL');
+insert into `jindong` (`number`, `price`, `title`) values('41','79','富贵鸟衣服男士夏季韩版潮流短袖t恤男装休闲上衣夏天青年翻领Polo透气体恤衫 深灰 M');
+insert into `jindong` (`number`, `price`, `title`) values('42','138','（买一送一）春季外套男士夹克2021连帽潮流青年工装上衣服男夹克男外套 1307-2130卡其+内搭T恤 XL');
+insert into `jindong` (`number`, `price`, `title`) values('43','95','引创短袖t恤男ins港风潮牌宽松个性时尚半袖韩版潮流百搭五分T恤衣服男 T086黑色 L');
+insert into `jindong` (`number`, `price`, `title`) values('44','99','快乐嘉人春秋外套男韩版潮流痞帅日系工装夹克ins港风学生秋季上衣服宽松 豆绿色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('45','48','李宁T恤男短袖衣服女运动服饰男士中国篮球polo衫瑜伽半袖体恤男装足球速干健身跑步装备暴汗紧身田径服 基础白-速干店长推荐 L(175)');
+insert into `jindong` (`number`, `price`, `title`) values('46','49','【三件套】卫衣男休闲运动套装2021春秋季男装男士青少年潮流休闲运动服饰情侣装卫衣套装衣服男 175黑三件套 XL');
+insert into `jindong` (`number`, `price`, `title`) values('47','69','【3件装】南极人短袖T恤男夏装新款时尚百搭男士半袖日系宽松体恤大码印花圆领透气衣服男装t恤上衣打底衫 【三件装】80小枫叶白+78小X黑+82小叶黑 XL');
+insert into `jindong` (`number`, `price`, `title`) values('48','128','南极人短袖t恤男纯棉夏季男士半袖衣服圆领休闲潮流打底上衣男生运动T恤衫汗衫3件装 白色H+黑色枫叶+白色英文 XL');
+insert into `jindong` (`number`, `price`, `title`) values('49','85','稻草人短袖t恤男夏季圆领半袖韩版潮流宽松男士上衣青少年学生ins风体恤港风衣服男装  深灰  M');
+insert into `jindong` (`number`, `price`, `title`) values('50','59','茄克斯 外套男士2021新款春秋季翻领长袖工装外套夹克男休闲男装修身潮流学生帅气男士潮牌褂子上衣服 深灰色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('51','59','东方狮啸  中国风短袖t恤男士夏季新款薄款透气韩版潮流半袖上衣服休闲宽松国潮五分袖青少年潮牌体恤夏装 白色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('52','108','【上衣+裤子】男款短袖短裤夏天套装男生夏季韩版全套衣服男土一套男装潮流搭配工装裤 198工装裤+D07破洞短袖黑 XL');
+insert into `jindong` (`number`, `price`, `title`) values('53','69','艾伦伯顿 运动套装男健身服速干冰丝短袖夏季宽松薄款训练篮球装备T恤跑步衣服晨跑短裤 科幻宽松两件套【冰丝套装】 XL(145-165)斤');
+insert into `jindong` (`number`, `price`, `title`) values('54','169','啄木鸟短袖套装男2021夏季新款韩版潮流印花半袖体恤男装上衣修身青少年学生男衣服一套潮牌套装 浅灰色 M');
+insert into `jindong` (`number`, `price`, `title`) values('55','99','木林森短袖T恤男装polo新款潮流夏季商务休闲翻领条纹拼色打底衫上衣服男士体恤 上白下黑 L');
+insert into `jindong` (`number`, `price`, `title`) values('56','79','中国李宁短袖t恤男装运动服饰春夏季半袖男士圆领吸汗速干透气休闲polo衫文化衫服饰跑步健身衣服上衣 标准白(店长推荐) 男L/175');
+insert into `jindong` (`number`, `price`, `title`) values('57','79','【套装】特步运动套装男装春夏季新款服饰薄款男士圆领卫衣短袖短裤子速干运动服健身衣跑步休闲篮球服两件套 黑色-(有口袋)-【推荐】 L/175');
+insert into `jindong` (`number`, `price`, `title`) values('58','49','古莱登 夏季ins学生圆领nasa宽松短袖t恤男韩版潮流男士上衣服港风宇航员半袖男装大码胖子五分印花 太空人白 M[适合110-130斤]');
+insert into `jindong` (`number`, `price`, `title`) values('59','35.9','夹克男韩版修身春秋季大码运动休闲男士外套卫衣服上衣帅气潮流 黑色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('60','48','引创短袖t恤男港风潮牌夏季ins超火韩版百搭半袖体恤宽松圆领印花衣服男 白色 L');
+insert into `jindong` (`number`, `price`, `title`) values('61','79','PBOA连帽衫卫衣男士春秋季加绒潮牌宽松长袖T恤运动套装大码情侣圆领加厚青少年男生款外套休闲衣服男装 8113白色（单件薄款） L');
+insert into `jindong` (`number`, `price`, `title`) values('62','99','南极人夹克男外套连帽秋季男生潮流休闲宽松运动衣服男装男士秋装 灰色.. XL');
+insert into `jindong` (`number`, `price`, `title`) values('63','38','奢号男生短袖夏季新款潮牌日系卡通上衣服潮流宽松ins港风体半袖T恤衫 黑色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('64','69','安踏运动短袖t恤男装上衣2021夏季圆领休闲男士运动服翻领POLO衫弹力透气半袖速干T恤健身衣男 云雾灰-店长推荐 L/175');
+insert into `jindong` (`number`, `price`, `title`) values('65','69','询彧 短袖T恤男夏季新款t恤半袖男士潮流衣服男装宽松打底衫五分袖运动休闲服饰短袖男 1927灰色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('66','128','南极人【超值2件装】短袖T恤男2021春季新款男士圆领短袖上衣男韩版修身青年潮流男装t恤半袖衣服男 6502灰色+6502灰色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('67','138','【超值 3件装】夹克男外套 春装季新款男士韩版修身连帽男装季工装运动套装男上衣服 JK601月蓝色(三件套) XL.');
+insert into `jindong` (`number`, `price`, `title`) values('68','49','保罗歌仕顿 短袖t恤男夏季polo衫男士翻领短袖T恤韩版潮流修身半袖衣服男装 灰色黑 XL');
+insert into `jindong` (`number`, `price`, `title`) values('69','99','南极人外套男夹克春秋季2021新款春装男士上衣ins潮韩版潮流学生休闲男装衣服工装春季春款 1938-卡其色 XL（115-130斤）');
+insert into `jindong` (`number`, `price`, `title`) values('70','138','南极人 外套男春秋夹克男2021新款休闲男士夹克衫韩版修身青年男装连帽工装外套学生上衣拼接外套衣服男 黑色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('71','79','南极人外套男士春秋季韩版潮流工装夹克休闲2020新款秋冬秋装上衣服 月蓝色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('72','169','啄木鸟夏季套装男短袖T恤运动潮流青少年学生套装修身印花半袖t恤搭配长裤子一套夏天男套装衣服 浅灰色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('73','49','欣迈隆 短袖t恤男夏季半截袖衣服圆领休闲宽松潮流初中学生印花T恤胖青年大码白色体恤汗衫 1607白色 XL');
+insert into `jindong` (`number`, `price`, `title`) values('74','59','乔丹短袖男运动t恤圆领2021年夏季半袖大码上衣舒适透气跑步运动健身服百搭休闲男装 黑-左logo圆领【店长推荐】 XL');
+insert into `jindong` (`number`, `price`, `title`) values('75','99','热艳【四件装】短袖T恤男士夏季宽松潮牌半袖T纯棉男休闲百搭男装上衣潮流薄款五分袖打底衫男潮流纯色衣服 T28白色+T37黑色+T57黑色+T67白色 XL(建议1...');
+insert into `jindong` (`number`, `price`, `title`) values('76','138','胖子大码棒球服男外套春秋2021新款韩版潮流宽松百搭ins潮牌港风夹克上衣 黑色 XL 130-155斤宽松');
+insert into `jindong` (`number`, `price`, `title`) values('77','159','JEEP吉普短袖t恤男2021新款翻领宽松衣服男装时尚休闲打底T恤衫纯色 PS3013墨绿 XL码建议160-175斤左右');
+insert into `jindong` (`number`, `price`, `title`) values('78','139','古莱登【买一送一，包邮】秋季长袖t恤男士小衫韩版潮流薄款卫衣秋衣上衣服外穿t桖 长 珊瑚黑+鱼钩黑 2件装 2XL');
+insert into `jindong` (`number`, `price`, `title`) values('79','30','李宁T恤男短袖衣服女运动服饰男士中国篮球polo衫瑜伽半袖体恤男装足球速干健身跑步装备暴汗紧身田径服 新基础黑-韦德之道-店长推荐 L(175)');
+insert into `jindong` (`number`, `price`, `title`) values('80','69','邦伊芙2021春夏季短袖t恤女 纯棉夏季新款bf港风短袖T恤女学生韩版宽松休闲百搭半袖上衣服潮 紫色 L 建议101-115斤');
+insert into `jindong` (`number`, `price`, `title`) values('81','42','南极人【精梳棉4件装】男士短袖t恤男夏季运动内衣舒适无痕宽松透气休闲衣服基础打底衫套头男式背心汗衫夏 黑+白+白+灰 大码 适合120-160斤');
+insert into `jindong` (`number`, `price`, `title`) values('82','69.9','李宁运动套装男卫衣卫裤衣服裤子服饰中国篮球服男士足球服健身服跑步装备休闲高弹春秋品牌夏田径服 基础黑卫衣+收口卫裤 L(175)');
